ff9ffdf3b6d7ee02e3001b9e5a42a0ca7e146990
Zhong <zzhong9@glsn-imac-12.ur.rochester.edu>
2018-09-20 23:20:53 -0400

    HW3 initial

---
+++ HW3/LetterCruncher/LetterCruncher.swift
@@ -0,0 +1,54 @@
+//
+//  LetterCruncher.swift
+//  LetterCruncher
+//
+//  Created by: CSC214 Instructor on 9/16/18.
+//  Copyright © 2018 University of Rochester. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LetterCruncher {
+    
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var alphaDigits = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"]
+    let maxDigits = 7
+
+    init() {
+        
+        // To do:
+        // generate alphaDigits array from alphabet string
+    }
+    
+    /*
+     ** NOTE: On those old phones the numbers 0 and one were not mapped to any keys.
+     ** We compensate by adding 2 in our return.
+     */
+    func digitStringForLetter(_ letter: String) -> String? {
+        for (i, letters) in alphaDigits.enumerated() {
+            if nil != letters.range(of: letter, options: .caseInsensitive) {
+                return String(describing: i + 2)
+            }
+        }
+        return nil
+    }
+    
+    func isValidInput(inputString: String, characterCount: Int) -> Bool {
+        if characterCount < maxDigits {
+            return true
+        }
+        return false
+    }
+    
+    func makeDigitsString(inputString: String) -> String? {
+        var outputString = String()
+        for char in inputString {
+            if let digit = digitStringForLetter(String(char)) {
+                outputString.append(digit)
+            }
+        }
+        return outputString.count > 0 ? outputString : nil
+    }
+}
+
