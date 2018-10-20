b3dce3f9cadc290646dfe03b80fb61b51f4317ac
Zhong <zzhong9@glsn-imac-12.ur.rochester.edu>
2018-10-20 12:11:45 -0400

    fixed

--- HW6/Faves/ViewController.swift
+++ HW6/Faves/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UITableViewController {
     
     // data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
