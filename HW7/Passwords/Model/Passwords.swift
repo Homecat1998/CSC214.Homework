a102a15c01e6cd913ff46ccbbc41f1f10fb4687f
Homecat1998 <43022020+Homecat1998@users.noreply.github.com>
2018-11-10 21:00:30 -0500

    Update Passwords.swift
    hehe

--- HW7/Passwords/Model/Passwords.swift
+++ HW7/Passwords/Model/Passwords.swift
@@ -15,7 +15,7 @@ let kAppGroupBundleID           = "group.com.examples.Passwords"
  */
 let dAppVersion                 = "app_version"
 let dNumLaunches                = "num_launches"
-let dLastLaunch                 = "accessDate"
+let dAccessDate                 = "accessDate"
 
 
 struct PasswordItem: Equatable, Codable {
