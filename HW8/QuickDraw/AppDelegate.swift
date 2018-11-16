cda488f8a7bb7cb3e8ca019bc467a31cb47997eb
Zhong <zzhong9@itc-imac-24.ur.rochester.edu>
2018-11-16 14:25:28 -0500

    moving

--- HW8/QuickDraw/AppDelegate.swift
+++ HW8/QuickDraw/AppDelegate.swift
@@ -2,47 +2,52 @@
 //  AppDelegate.swift
 //  QuickDraw
 //
-//  Created by CSC214 Instructor on 8/6/18.
-//  Copyright © 2018 University of Rochester. All rights reserved.
+//  Created by Zhong, Zhetao on 11/15/18.
+//  Copyright © 2018 Zhong, Zhetao. All rights reserved.
 //
 
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
 
     var window: UIWindow?
     
     var draws : Draws!
-
+    
     var document = QDDocument(title: NSLocalizedString("str_untitled", comment: ""))
     
-    override init() {
-    }
-    
-    var dataFileName = "QDrawFile"
+    var dataFileName = "HeheFile"
     
     lazy var fileURL: URL = {
         let documentsDir =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         return documentsDir.appendingPathComponent(dataFileName, isDirectory: false)
     }()
 
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-                loadData()
+        loadData()
         
-        if let navController = window?.rootViewController as? UINavigationController,
-            let mainViewController = navController.viewControllers.first as? TableViewController {
-            mainViewController.draws = draws
+        // Override point for customization after application launch.
+        let splitViewController = window!.rootViewController as! UISplitViewController
+        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+        
+        // master
+        let navController = splitViewController.viewControllers.first as! UINavigationController
+        if let masterController = navController.topViewController as? MasterViewController{
+            
+            masterController.draws = draws
         }
+        
+        splitViewController.delegate = self
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        
         saveData()
     }
 
@@ -62,6 +67,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+
+    // MARK: - Split view
+
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+        if topAsDetailController.document == nil {
+            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+            return true
+        }
+        return false
+    }
+    
+    
+    
     
     
     // MARK - Persistence Methods
@@ -101,7 +121,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             fatalError("No data at \(fileURL.path)!")
         }
     }
-
 
 }
 
